a66a5eac809dd14580daab9b72e3ab4bb4636399
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-01-11 00:54:43 +0000

    Change function definition

--- functions.js
+++ functions.js
@@ -3,8 +3,7 @@
  * @returns a string of 6 random alphanumeric characters
  */
 function generateRandomString() {
-  for (var s=''; s.length < 6; s += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.charAt(Math.random()*62|0));
-  return s;
+  return Math.random().toString(16).slice(2, 8);
 }
 
 module.exports = { generateRandomString };
