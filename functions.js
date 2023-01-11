bc86092264a34cf8301b6282b100d54e4c90b490
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-01-11 04:02:37 +0000

    Move foundUserByEmail from express_server.js to here

--- functions.js
+++ functions.js
@@ -1,4 +1,3 @@
-
 /**
  * 
  * @returns a string of 6 random alphanumeric characters
@@ -7,6 +6,16 @@ const generateRandomString = function() {
   return Math.random().toString(16).slice(2, 8);
 };
 
+/**
+ * @returns user object if found, null if not found
+ */
+const foundUserByEmail = function(email) {
+  for (const userId in users) {
+    if (users[userId].email === email) {
+      return users[userId];
+    }
+  }
+  return null;
+};
 
-
-module.exports = { generateRandomString };
+module.exports = { generateRandomString, foundUserByEmail };
